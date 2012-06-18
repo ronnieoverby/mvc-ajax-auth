b099314b596635cb47382fe28c20ad07777796a1
Ronnie Overby <roverby@core-techs.net>
2012-06-18 10:14:51 -0400

    init commit

---
+++ MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        [AjaxAuthorize]
+        public ActionResult Secret()
+        {
+            return PartialView();
+        }
+    }
+}
